85c96e3f7f2a644d7de73007462d9f172850c269
William RAD Funk <contact@WILLIAMRADFunk.com>
2019-11-04 21:20:24 -0800

    Finishes experimenting with twojs. TwoJS still needs ALOT of work before it's work investing the effort. ThreeJS still takes the cake, even for 2D works.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # desktop-kitten
-Use/Experiment with twoJs library to make an automated kitten in local browser on desktop.
+Use/Experiment with twoJs library to make a kitten in browser.
+
+TwoJs feels to cumbersome, and the typescript-wrapped version has too many gaps for this to be a valid game library option. ThreeJs made to work in 2D still seems the superior choice.
--- src/ts/init.ts
+++ src/ts/init.ts
@@ -1,25 +1,12 @@
 import * as Two from 'twojs-ts';
-import { Example } from './example';
 /**
  * Placeholder function typically used to initiate the applications loop.
  */
 export default () => {
-    const example = new Example();
-    const h1El = document.getElementsByTagName('h1');
-    h1El[0].innerHTML = `${example.getName()} of a Desktop Kitten`;
-
     const elem = document.getElementById('mainview');
-    const two = new Two({ width: 600, height: 600 }).appendTo(elem);
-
-    const circle = two.makeCircle(-70, 0, 50);
-    const rect = two.makeRectangle(70, 0, 100, 100);
-    circle.fill = '#FF8000';
-    rect.fill = 'rgba(0, 200, 255, 0.75)';
-
-    const group1 = two.makeGroup([circle, rect]);
-    group1.translation.set(two.width / 2, two.height / 2);
-    group1.scale = 0;
-    group1.noStroke();
+    const body = document.getElementsByTagName('body')[0];
+    const two = new Two({ width: body.offsetWidth, height: body.offsetHeight }).appendTo(elem);
+    const center = [two.width / 2, two.height / 2];
 
     const ear1 = two.makeRectangle(0, 0, 30, 30);
     ear1.fill = '#333333';
@@ -27,26 +14,188 @@ export default () => {
     ear1Inner.fill = '#DDDDDD';
 
     const group2 = two.makeGroup([ear1, ear1Inner]);
-    group2.translation.set(350, 400);
     group2.rotation = 0.4;
+    group2.translation.set(center[0] - 25, center[1] - 215);
 
-    const head = two.makeEllipse(300, 500, 50, 25);
+    const ear2 = two.makeRectangle(0, 0, 30, 30);
+    ear2.fill = '#333333';
+    const ear2Inner = two.makeRectangle(0, 0, 20, 20);
+    ear2Inner.fill = '#DDDDDD';
+
+    const group3 = two.makeGroup([ear2, ear2Inner]);
+    group3.rotation = -0.4;
+    group3.translation.set(center[0] + 25, center[1] - 215);
+
+    const bodyOuter = two.makeRoundedRectangle(center[0], center[1] - 150, 60, 100, 20);
+    bodyOuter.fill = '#333333';
+
+    const bodyInner = two.makeRoundedRectangle(center[0], center[1] - 150, 40, 80, 20);
+    bodyInner.fill = '#FFFFFF';
+
+    const head = two.makeEllipse(center[0], center[1] - 200, 50, 25);
     head.fill = '#333333';
-    // const ear1 = two.makePath([250, 250, 300, 250, 250, 250], false);
-    // ear1.fill = '#333333';
-    // const ear2 = two.makePath([275, 275, 325, 275, 275, 275], false);
-    // ear2.fill = '#333333';
+    const eyeLeftOuter = two.makeEllipse(center[0] - 20, center[1] - 200, 10, 5);
+    eyeLeftOuter.fill = '#307047';
+    const eyeLeftInner = two.makeEllipse(center[0] - 20, center[1] - 200, 3, 4);
+    eyeLeftInner.fill = '#000000';
+    const eyeRightOuter = two.makeEllipse(center[0] + 20, center[1] - 200, 10, 5);
+    eyeRightOuter.fill = '#307047';
+    const eyeRightInner = two.makeEllipse(center[0] + 20, center[1] - 200, 3, 4);
+    eyeRightInner.fill = '#000000';
 
-    // Bind a function to scale and rotate the group
-    // to the animation loop.
-    two.bind(Two.Events.update, () => {
-        // This code is called everytime two.update() is called.
-        // Effectively 60 times per second.
-        if (group1.scale > 0.9999) {
-            group1.scale = group1.rotation = 0;
-        }
-        const t = (1 - group1.scale) * 0.125;
-        group1.scale += t;
-        group1.rotation += t * 4 * Math.PI;
-    }).play();  // Finally, start the animation loop
+    const tongueOuter = two.makeEllipse(center[0], center[1] - 181, 4, 6);
+    tongueOuter.fill = '#FF1402';
+
+    const mouthLeft1 = two.makeLine(center[0] - 5, center[1] - 182, center[0], center[1] - 187);
+    mouthLeft1.linewidth = 2;
+    mouthLeft1.fill = '#666666';
+    mouthLeft1.noFill();
+
+    const mouthLeft2 = two.makeLine(center[0] - 5, center[1] - 182, center[0] - 10, center[1] - 182);
+    mouthLeft2.linewidth = 2;
+    mouthLeft2.fill = '#666666';
+    mouthLeft2.noFill();
+
+    const mouthRight1 = two.makeLine(center[0] + 5, center[1] - 182, center[0], center[1] - 187);
+    mouthRight1.linewidth = 2;
+    mouthRight1.fill = '#666666';
+    mouthRight1.noFill();
+
+    const mouthRight2 = two.makeLine(center[0] + 5, center[1] - 182, center[0] + 10, center[1] - 182);
+    mouthRight2.linewidth = 2;
+    mouthRight2.fill = '#666666';
+    mouthRight2.noFill();
+
+    const toungueLine = two.makeLine(center[0], center[1] - 177, center[0], center[1] - 183);
+    toungueLine.linewidth = 1;
+    toungueLine.opacity = 0.3;
+    toungueLine.fill = '#666666';
+
+    const nose = two.makeRoundedRectangle(center[0], center[1] - 193, 10, 7, 3);
+    nose.fill = '#E75480';
+
+    const leftLeg = two.makeRoundedRectangle(center[0] - 25, center[1] - 125, 20, 50, 10);
+    leftLeg.fill = '#333333';
+    const groupLegLeft = two.makeGroup([leftLeg]);
+    // groupLegLeft.rotation = 0.1;
+    // groupLegLeft.translation.set(30, -60);
+
+    const leftLegPaw = two.makeRoundedRectangle(center[0] - 25, center[1] - 100, 18, 10, 5);
+    leftLegPaw.fill = '#FFFFFF';
+
+    const rightLeg = two.makeRoundedRectangle(center[0] + 25, center[1] - 125, 20, 50, 10);
+    rightLeg.fill = '#333333';
+    const groupLegRight = two.makeGroup([rightLeg]);
+    // groupLegRight.rotation = -0.1;
+    // groupLegRight.translation.set(-25, 75);
+
+    const rightLegPaw = two.makeRoundedRectangle(center[0] + 25, center[1] - 100, 18, 10, 5);
+    rightLegPaw.fill = '#FFFFFF';
+
+    const leftLegPawLine1 = two.makeLine(center[0] - 22, center[1] - 105, center[0] - 22, center[1] - 99);
+    leftLegPawLine1.linewidth = 1;
+    leftLegPawLine1.opacity = 0.5;
+    leftLegPawLine1.fill = '#CCCCCC';
+
+    const leftLegPawLine2 = two.makeLine(center[0] - 28, center[1] - 105, center[0] - 28, center[1] - 99);
+    leftLegPawLine2.linewidth = 1;
+    leftLegPawLine2.opacity = 0.5;
+    leftLegPawLine2.fill = '#CCCCCC';
+
+    const rightLegPawLine1 = two.makeLine(center[0] + 22, center[1] - 105, center[0] + 22, center[1] - 99);
+    rightLegPawLine1.linewidth = 1;
+    rightLegPawLine1.opacity = 0.5;
+    rightLegPawLine1.fill = '#CCCCCC';
+
+    const rightLegPawLine2 = two.makeLine(center[0] + 28, center[1] - 105, center[0] + 28, center[1] - 99);
+    rightLegPawLine2.linewidth = 1;
+    rightLegPawLine2.opacity = 0.5;
+    rightLegPawLine2.fill = '#CCCCCC';
+
+    const leftArm = two.makeRoundedRectangle(center[0] - 25, center[1] - 155, 15, 35, 7);
+    leftArm.fill = '#333333';
+
+    const leftArmPaw = two.makeRoundedRectangle(center[0] - 25, center[1] - 142, 15, 10, 5);
+    leftArmPaw.fill = '#FFFFFF';
+
+    const leftArmPawLine1 = two.makeLine(center[0] - 23, center[1] - 142, center[0] - 23, center[1] - 137);
+    leftArmPawLine1.linewidth = 1;
+    leftArmPawLine1.opacity = 0.5;
+    leftArmPawLine1.fill = '#CCCCCC';
+
+    const leftArmPawLine2 = two.makeLine(center[0] - 27, center[1] - 142, center[0] - 27, center[1] - 137);
+    leftArmPawLine2.linewidth = 1;
+    leftArmPawLine2.opacity = 0.5;
+    leftArmPawLine2.fill = '#CCCCCC';
+
+    const rightArm = two.makeRoundedRectangle(center[0] + 25, center[1] - 155, 15, 35, 7);
+    rightArm.fill = '#333333';
+
+    const rightArmPaw = two.makeRoundedRectangle(center[0] + 25, center[1] - 142, 15, 10, 5);
+    rightArmPaw.fill = '#FFFFFF';
+
+    const rightArmPawLine1 = two.makeLine(center[0] + 23, center[1] - 142, center[0] + 23, center[1] - 137);
+    rightArmPawLine1.linewidth = 1;
+    rightArmPawLine1.opacity = 0.5;
+    rightArmPawLine1.fill = '#CCCCCC';
+
+    const rightArmPawLine2 = two.makeLine(center[0] + 27, center[1] - 142, center[0] + 27, center[1] - 137);
+    rightArmPawLine2.linewidth = 1;
+    rightArmPawLine2.opacity = 0.5;
+    rightArmPawLine2.fill = '#CCCCCC';
+
+    const tailTip = two.makeEllipse(center[0] - 85, center[1] - 180, 4.5, 4.5);
+    tailTip.stroke = '#87CEEB';
+    tailTip.fill = '#333333';
+
+    const tail = two.makeCurve(
+        center[0] - 85, center[1] - 180,
+        center[0] - 75, center[1] - 180,
+        center[0] - 55, center[1] - 120,
+        center[0] - 35, center[1] - 150,
+        center[0] - 15, center[1] - 120,
+        center[0], center[1] - 150, true);
+    tail.linewidth = 8;
+    tail.stroke = '#333333';
+    tail.noFill();
+
+    const groupCat = two.makeGroup([
+        group2,
+        group3,
+        tailTip,
+        tail,
+        bodyOuter,
+        bodyInner,
+        head,
+        eyeLeftOuter,
+        eyeLeftInner,
+        eyeRightOuter,
+        eyeRightInner,
+        tongueOuter,
+        mouthLeft1,
+        mouthLeft2,
+        mouthRight1,
+        mouthRight2,
+        nose,
+        toungueLine,
+        groupLegLeft,
+        groupLegRight,
+        leftLegPaw,
+        rightLegPaw,
+        leftLegPawLine1,
+        leftLegPawLine2,
+        rightLegPawLine1,
+        rightLegPawLine2,
+        leftArm,
+        leftArmPaw,
+        leftArmPawLine1,
+        leftArmPawLine2,
+        rightArm,
+        rightArmPaw,
+        rightArmPawLine1,
+        rightArmPawLine2]);
+    groupCat.scale = 2;
+    groupCat.translation.set(-center[0], 0);
+
+    two.bind(Two.Events.update, () => {}).play();
 }
